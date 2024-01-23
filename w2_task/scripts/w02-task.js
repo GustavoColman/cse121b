3323684de7f5671160a2559b8a75b4f9ad5c89d7
GustavoColman <luizgustavocolman700@gmail.com>
2024-01-23 15:32:42 -0300

    code adjust

---
+++ w2_task/scripts/w02-task.js
@@ -0,0 +1,50 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname=" Gustavo Colman";
+
+let currentYear="2024";
+
+let profilePicture = "w2_task\images\colmanPicture.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+
+let foodelement = document.getElementById('food');
+
+let yearElement = document.querySelector('#year');
+
+let imageElement = profilePicture ;
+
+
+
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+
+yearElement.textContent = `${currentYear}`;
+
+imageElement.setAttribute('src',profilePicture);
+
+foodelement.innerHTML += `<br>${favFood}`;
+
+
+
+
+
+
+
+/* Step 5 - Array */
+var favFood = ["pizza","Hamburger","coffee with milk"];
+
+
+// só preciso fazer a lista de comidas e fazer a imagem aparecer agora
+
+
+
+
